0d75e85a0f5024cd7a7e8026a63dff3be345349a
nguyenhuuphong2511 <nguyenhuuphong2511@gmail.com>
2021-10-05 23:18:05 +0700

    Fix sản phẩm

--- project/src/main/java/com/repository/SanPhamDAO.java
+++ project/src/main/java/com/repository/SanPhamDAO.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.model.SanPham;
 
 @Repository
-public interface SanPhamDAO  extends JpaRepository<SanPham, Long> {
+public interface SanPhamDAO  extends JpaRepository<SanPham, Integer> {
 
 }
