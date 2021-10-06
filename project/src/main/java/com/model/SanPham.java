36a7366378f6baa88e9b8e98ef44ff9820f19588
nguyenhuuphong2511 <nguyenhuuphong2511@gmail.com>
2021-10-06 16:52:09 +0700

    Update SanPham.java
    san pham ctl

--- project/src/main/java/com/model/SanPham.java
+++ project/src/main/java/com/model/SanPham.java
@@ -19,4 +19,54 @@ public class SanPham implements Serializable{
 	String mota;
 	Boolean tinhtrang;
 	String hinh;
+	
+	public Integer getMasp() {
+		return masp;
+	}
+	public void setMasp(Integer masp) {
+		this.masp = masp;
+	}
+	public String getTensp() {
+		return tensp;
+	}
+	public void setTensp(String tensp) {
+		this.tensp = tensp;
+	}
+	public Integer getMaloai() {
+		return maloai;
+	}
+	public void setMaloai(Integer maloai) {
+		this.maloai = maloai;
+	}
+	public Integer getManh() {
+		return manh;
+	}
+	public void setManh(Integer manh) {
+		this.manh = manh;
+	}
+	public Double getGia() {
+		return gia;
+	}
+	public void setGia(Double gia) {
+		this.gia = gia;
+	}
+	public String getMota() {
+		return mota;
+	}
+	public void setMota(String mota) {
+		this.mota = mota;
+	}
+	public Boolean getTinhtrang() {
+		return tinhtrang;
+	}
+	public void setTinhtrang(Boolean tinhtrang) {
+		this.tinhtrang = tinhtrang;
+	}
+	public String getHinh() {
+		return hinh;
+	}
+	public void setHinh(String hinh) {
+		this.hinh = hinh;
+	}
+	
 }
