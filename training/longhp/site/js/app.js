3aeb95b229e13e7d0a56e5d3f69acdf749c3fe41
Hoàng Phi Long <longhpdev18@gmail.com>
2021-10-07 22:00:47 +0700

    UPDATE SITE

---
+++ training/longhp/site/js/app.js
@@ -0,0 +1,9 @@
+$('.btn-showLogin').click(function() {
+    $('.formLogin__container').addClass('active');
+})
+$('.btn-close').click(function() {
+    $('.formLogin__container').removeClass('active');
+})
+$('.btn-cancel').click(function() {
+    $('.formLogin__container').removeClass('active');
+})
