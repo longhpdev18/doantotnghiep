9c356ed93d02851962c191c65aa3c46a623fe316
danhlncps10076 <66855948+danhlncps10076@users.noreply.github.com>
2021-10-09 22:09:53 +0700

    update

--- project/src/main/resources/static/js/app.js
+++ project/src/main/resources/static/js/app.js
@@ -1,13 +1,36 @@
 $('.btn-showLogin').click(function() {
-    $('.formLogin__container').addClass('active');
+	$('.formLogin__container').addClass('active');
 })
 $('.btn-close').click(function() {
-    $('.formLogin__container').removeClass('active');
+	$('.formLogin__container').removeClass('active');
 })
 $('.btn-cancel').click(function() {
-    $('.formLogin__container').removeClass('active');
+	$('.formLogin__container').removeClass('active');
 })
 
 $('.btn-showUser').click(function() {
-    $('.dropdown-user').toggle('active');
+	$('.dropdown-user').toggleClass('active');
 })
+
+	window.addEventListener("scroll", function() {
+		var x = pageYOffset;
+		if (x > 300) {
+			$('.btn-cat').mouseover(function() {
+				
+				$('.menu__container').addClass('active');
+			})
+			$('.btn-cat').mouseout(function() {
+				
+				$('.menu__container').removeClass('active');
+			})
+			
+		}
+		else {
+			$('.btn-cat').hover(function() {
+
+				$('.menu__container').removeClass('active');
+			})
+
+		}
+
+	})
---
+++ project/src/main/webapp/WEB-INF/view/js/app.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  var x = document.getElementsByClassName("menu_top");
+	x.style.display = none;
+}
+myFunction();
