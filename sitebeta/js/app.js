b238904f2b53e26952e25cf975e7782029cd681d
Hoàng Phi Long <longhpdev18@gmail.com>
2021-10-12 21:29:22 +0700

    UPDATE FROM REGISTER

--- sitebeta/js/app.js
+++ sitebeta/js/app.js
@@ -1,11 +1,21 @@
 $('.btn-showLogin').click(function () {
     $('.formLogin__container').addClass('active');
 })
+$('.btn-toLogin').click(function () {
+    $('.formLogin__container').addClass('active');
+    $('.formRegister__container').removeClass('active');
+})
+$('.btn-toRegister').click(function () {
+    $('.formRegister__container').addClass('active');
+    $('.formLogin__container').removeClass('active');
+})
 $('.btn-close').click(function () {
     $('.formLogin__container').removeClass('active');
+    $('.formRegister__container').removeClass('active');
 })
 $('.btn-cancel').click(function () {
     $('.formLogin__container').removeClass('active');
+    $('.formRegister__container').removeClass('active');
 })
 
 $('.btn-showUser').click(function () {
