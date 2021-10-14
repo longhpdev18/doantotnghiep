40322c0d4372f57f2af83e4c77f1d68295a5fd1d
Trang Tran Tuan Kiet <trangtrantuankiet@gmail.com>
2021-10-14 20:27:49 +0700

    update database

---
+++ database/insert2.sql
@@ -0,0 +1,17 @@
+﻿use DOAN
+go
+
+--------
+
+--Nhân viên--
+insert into nhanvien values ('admin','admin123','0',N'Trang Trần Tuấn Kiệt','1999-08-05','0',N'Bình Dương','trangtrantuankiet@gmail.com','0968804871','img.jpg')
+--Khách hàng--
+insert into khachhang values ('khachhang1','0123456789',N'Nguyễn Văn A','2000-01-01','0',N'Hồ Chí Minh','0905497631','nguyenvana@gmail.com','1','img2.jpg')
+--Hóa đơn--
+insert into hoadon values ('1','2021-10-14',N'Đã nhận hàng')
+--Hóa đơn chi tiết--
+insert into hoadonchitiet values('1','2','3','2300000')
+--Đánh giá--
+insert into danhgia values('1','2',N'Sản phẩm tốt, quá tuyệt vời')
+
+
