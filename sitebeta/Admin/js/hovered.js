c77f2fc443e3bbac5ed59281dd86532b6555fcf3
= <=>
2021-11-04 09:16:54 +0700

    admin

---
+++ sitebeta/Admin/js/hovered.js
@@ -0,0 +1,25 @@
+//thêm lớp được di chuột vào một danh mục đã chọn
+let list = document.querySelectorAll('.navigation li');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
+}
+list.forEach((item) =>
+    item.addEventListener('mouseover', activeLink));
+//menuToggle 
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+
+    toggle.onclick = function(){
+        navigation.classList.toggle('active');
+        main.classList.toggle('active');
+    }
+//click all checkbox 
+function checkUncheck(main){
+    all = document.getElementsByName('lang');
+    for(var a = 0; a<all.length; a++) {
+        all[a].checked = main.checked;
+    }
+}
