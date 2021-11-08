935ea049cade635964928e6ea7da7041f35082cd
nguyenhuuphong2511 <nguyenhuuphong2511@gmail.com>
2021-11-08 22:30:03 +0700

    up

--- project/src/main/java/com/controller/IndexController.java
+++ project/src/main/java/com/controller/IndexController.java
@@ -64,7 +64,7 @@ public class IndexController {
 				sessionService.set("maKH", item.getMakh());
 			}
 		}
-		return "redirect://";
+		return "redirect:/";
 	}
 	
 //	@RequestMapping("/product/page")
