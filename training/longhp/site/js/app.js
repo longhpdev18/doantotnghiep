d854e43da64072086e1f63eef51bbd2e9712bb04
Hoàng Phi Long <longhpdev18@gmail.com>
2021-11-11 15:45:38 +0700

    UPDATE CODE

--- training/longhp/site/js/app.js
+++ training/longhp/site/js/app.js
@@ -33,6 +33,59 @@ $('.btn-mobile-searchbox').click(function () {
     $('.mb-search').toggleClass('active');
 })
 
+$(document).ready(function () {
+    $('.product-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        arrows: true,
+        draggable: false,
+        prevArrow: "<button type='button' class='slick-prev slick-button pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next  slick-button pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.hero-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows: true,
+        draggable: false,
+        prevArrow: "<button type='button' class='hero-slick-prev hero-slick-button pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='hero-slick-next  hero-slick-button pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
+    });
+
+});
+
 
 
 
